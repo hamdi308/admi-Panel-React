df33907fa8eab27a3184db367b614a7debfd6557
hamdi hadda <81825150+hamdi308@users.noreply.github.com>
2022-10-08 16:09:42 +0100

    add widgets

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,19 @@
 import NavBar from '../../components/NavBar/NavBar';
 import SideBar from '../../components/SideBar/SideBar';
+import Widget from '../../components/Widget/Widget';
 import './index.scss';
 const Home = () => {
   return (
       <div className='home'>
           <SideBar />
           <div className='homeContainer'>
-        <NavBar />
-        home container
+              <NavBar />
+        <div className='widgets'>
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+        </div>
           </div>
     </div>
   )
