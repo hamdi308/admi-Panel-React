d3aa921874db9c0afcb679e5a2e47f5498802a65
hamdi hadda <81825150+hamdi308@users.noreply.github.com>
2022-10-09 17:11:41 +0100

    add page of adding new User&Product

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./Pages/LoginPage/Login";
 import List from "./Pages/List/List";
 import Single from "./Pages/Single/Single";
 import New from "./Pages/NewPage/New";
+import { userInputs,productInputs } from "./formSource";
 function App() {
   return (
     <Router>
@@ -15,12 +16,12 @@ function App() {
         <Route path="users">
           <Route index element={<List />} />
           <Route path=":userId" element={<Single/>} />
-          <Route path="new" element={<New/>} />
+          <Route path="new" element={<New inputs={ userInputs} title="Add New User"/>} />
         </Route>
          <Route path="products">
           <Route index element={<List />} />
           <Route path=":productId" element={<Single/>} />
-          <Route path="new" element={<New/>} />
+          <Route path="new" element={<New inputs={ productInputs } title="Add New Product"/>} />
         </Route>
       </Routes>
     </Router>
---
+++ src/formSource.jsx
@@ -0,0 +1,76 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "hamdi_hadda",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "Hamdi Hadda",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "haddahamdi308@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+216 94365066",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Bekalta, Monastir",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "Tunisia",
+    },
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Title",
+      type: "text",
+      placeholder: "Iphone 7 plus",
+    },
+    {
+      id: 2,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 3,
+      label: "Category",
+      type: "text",
+      placeholder: "SmartPhone",
+    },
+    {
+      id: 4,
+      label: "Price",
+      type: "text",
+      placeholder: "600",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
